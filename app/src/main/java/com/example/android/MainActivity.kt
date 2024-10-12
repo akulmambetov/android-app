41847f8ef0d82ecdda95eea860cc2edcb956cefe
Atai Kulmambetov <atai.k@intelliants.com>
2024-10-12 20:18:58 +0600

    ci: update gradle package

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.android.ui.theme.AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +34,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = Modifier.padding(24.dp)) {
+        Text(text = "Hello,")
+        Text(text = name)
+    }
 }
 
 @Preview(showBackground = true)
